5ab47bb356522ec1ad13ab4b3b55f5a53f086c67
Olarewaju Opeyemi <olarewajuakeemope@icloud.com>
2017-11-07 11:23:34 +0100

    Replace map with forEach and cleanup

--- public/javascripts/carousel.js
+++ public/javascripts/carousel.js
@@ -1,5 +1,4 @@
 // Backbone Model
-
 var Slide = Backbone.Model.extend({
   defaults: {
     images: [],
@@ -8,27 +7,24 @@ var Slide = Backbone.Model.extend({
 });
 
 // Backbone Collection
-
 var Slides = Backbone.Collection.extend({
   url: '/slides',
   parse: function(data) {
-    data.slides.map(function(slide, index) {
+    data.slides.forEach(function(slide, index) {
       slide.url = '';
-      slide.index = 0;
+      slide.index = index;
     });
     return data.slides;
   },
-  initialize: function(){
+  initialize: function() {
     this.fetch();
   },
 });
 
 // Instantiate a Collection
-
 var slides = new Slides();
 
 // Backbone Single Slide View
-
 var SlideView = Backbone.View.extend({
   model: new Slide(),
   tagName: 'div',
@@ -59,7 +55,6 @@ var SlideView = Backbone.View.extend({
 });
 
 // Backbone All Slides View
-
 var SlidesView = Backbone.View.extend({
   model: slides,
   el: $('#carousel'),
@@ -68,30 +63,24 @@ var SlidesView = Backbone.View.extend({
   },
   render: function() {
     var self = this;
-    var modelCount = 0;
     this.$el.html('');
     _.each(this.model.toArray(), function(slide) {
       var images = slide.get('images');
       var randomInt = Math.floor(Math.random() * 3);
       self.$el.append((new SlideView({ model: slide })).render().$el);
-      slide.set('index', modelCount);
       slide.set('url', images[randomInt]);
-      modelCount += 1;
     });
     return this;
   },
 });
 
 // Instantiate All Slides View
-
 var slidesView = new SlidesView();
 
 // Instantiate script
-
 $(document).ready(function() {
-  setTimeout(function(){
+  setTimeout(function() {
     $('.carousel-prev:first').prop('disabled', true);
     $('.carousel-next:last').prop('disabled', true);
   }, 200);
-
 });
